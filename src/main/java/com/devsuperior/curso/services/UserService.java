6aea074ca60e73522f829920e345433d802fbd3b
Gustavo de França <gustavonota20@hotmail.com>
2023-10-03 15:17:36 -0300

    Tablhando os registros

---
+++ src/main/java/com/devsuperior/curso/services/UserService.java
@@ -0,0 +1,24 @@
+package com.devsuperior.curso.services;
+
+import com.devsuperior.curso.entities.User;
+import com.devsuperior.curso.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+    public User findById(Long id){
+        Optional<User> obj= repository.findById(id);
+        return obj.get();
+    }
+}
