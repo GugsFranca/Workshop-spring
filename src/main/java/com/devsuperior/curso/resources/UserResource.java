0ef6417566fed3c52df4441b37356410059e8446
Gustavo de França <gustavonota20@hotmail.com>
2023-10-03 11:44:16 -0300

    Entidade e recurso do Usuario

---
+++ src/main/java/com/devsuperior/curso/resources/UserResource.java
@@ -0,0 +1,18 @@
+package com.devsuperior.curso.resources;
+
+import com.devsuperior.curso.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value ="/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User(1L, "Maria", "maria@gmail.com", "999999", "12354");
+        return ResponseEntity.ok().body(u);
+    }
+}
