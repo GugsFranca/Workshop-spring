9e2ddbd8f85c2f8f066969555fd9f765026aeb36
Gustavo de França <gustavonota20@hotmail.com>
2023-10-04 11:23:05 -0300

    Criando tabela Order e fazendo o relacionamento dela com User

--- src/main/java/com/devsuperior/curso/entities/User.java
+++ src/main/java/com/devsuperior/curso/entities/User.java
@@ -1,11 +1,10 @@
 package com.devsuperior.curso.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity(name = "tb_user")
 public class User implements Serializable {
@@ -17,7 +16,10 @@ public class User implements Serializable {
     private String phone;
     private String password;
 
-    public User(){
+    @OneToMany(mappedBy = "client")
+    private static List<Order> orders = new ArrayList<>();
+
+    public User() {
 
     }
 
@@ -27,6 +29,10 @@ public class User implements Serializable {
         this.email = email;
         this.phone = phone;
         this.password = password;
+    }
+
+    public static List<Order> getOrders() {
+        return orders;
     }
 
     public Long getId() {
