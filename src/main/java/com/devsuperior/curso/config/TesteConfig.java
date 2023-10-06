1d9f4ac4b052f3c4845562f1b8f0ea1d1bef0df6
Gustavo de França <gustavonota20@hotmail.com>
2023-10-06 12:18:24 -0300

    Tabela de pagamento

--- src/main/java/com/devsuperior/curso/config/TesteConfig.java
+++ src/main/java/com/devsuperior/curso/config/TesteConfig.java
@@ -69,7 +69,12 @@ public class TesteConfig implements CommandLineRunner {
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+
         orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+
+        Payment pay1 = new Payment(null, Instant.parse("2019-06-20T19:53:07Z"), o1);
+        o1.setPayment(pay1);
+        orderRepository.save(o1);
 
     }
 }
