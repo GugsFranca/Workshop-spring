9e98a11f2e5652db4c6d3dfb225744c252c1662c
Gustavo de França <gustavonota20@hotmail.com>
2023-10-11 11:26:40 -0300

    Tratando exceções -findById

--- src/main/java/com/devsuperior/curso/services/UserService.java
+++ src/main/java/com/devsuperior/curso/services/UserService.java
@@ -2,6 +2,7 @@ package com.devsuperior.curso.services;
 
 import com.devsuperior.curso.entities.User;
 import com.devsuperior.curso.repositories.UserRepository;
+import com.devsuperior.curso.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,7 +20,7 @@ public class UserService {
     }
     public User findById(Long id){
         Optional<User> obj= repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
